8c5f7de67142095ce54c27884eb3121f2835a1b1
Noor Fadhila Fadhal Ahmad <fadilby89@gmail.com>
2018-06-12 15:43:05 +0800

    penambahan yg lain

---
+++ application/controllers/Candah.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Candah extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('candah/dasboard');
+        
+    }
+
+}
+
+/* End of file Candah.php */
---
+++ application/models/M_jai.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_jai extends CI_Model {
+
+    
+
+}
+
+/* End of file M_jai.php */
